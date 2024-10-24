8febc09e896a3715a330bdf8d4a8d63706582aa8
Kiran Salve <90862340+kiransalve@users.noreply.github.com>
2024-10-24 12:55:01 +0530

    projects images3

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,50 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import { SiPowerbi, SiMicrosoftexcel, SiPython, SiMysql } from "react-icons/si";
-import hr from "../../img/hr.png";
-import adi from "../../img/adi_sales.png";
-import blinkit from "../../img/blinkit.png";
-import coffee from "../../img/coffee.png";
-
-const projectData = [
-  {
-    title: "HR Dashboard",
-    subtitle:
-      "Monitor workforce metrics, employee performance by drill through functionality",
-    githubLink: "https://github.com/salvekiran/PowerBI_HR_Dashboard",
-    imgUrl: hr,
-    videoUrl: "bXuYiLul91s",
-    skill: "Power BI",
-  },
-  {
-    title: "Adidas Sales Dashboard",
-    subtitle:
-      "Visualize sales performance, track revenue, profits by multiple measures same like filter.",
-    githubLink: "https://github.com/salvekiran/PowerBI_Sales_Dashboard_Aadidas",
-    imgUrl: adi,
-    videoUrl: "yXmE4e3dGKQ",
-    skill: "Power BI",
-  },
-  {
-    title: "Blinkit Store Sales Dashboard",
-    subtitle:
-      "Track store sales, inventory, revenue, product performance, and customer trends.",
-    githubLink: "https://github.com/salvekiran/PowerBI_Sales_Dashboard",
-    imgUrl: blinkit,
-    videoUrl: "cbdvQUdvBIo",
-    skill: "Power BI",
-  },
-  {
-    title: "Coffee Shop Sales Dashboard",
-    subtitle:
-      "See details of monthly sales, product performance and daily/hourly basis.",
-    githubLink:
-      "https://github.com/salvekiran/PowerBI_CofeeShop_Sales_Dashboard",
-    imgUrl: coffee,
-    videoUrl: "nivoWqNOlPo",
-    skill: "Power BI",
-  },
-];
+import { projectData } from "../../data/projectdata";
 
 const skillData = [
   { Icon: SiPowerbi, title: "Power BI" },
