0a0bdf6027bb26a65dd0263f9035a290230ee001
Kiran Salve <90862340+kiransalve@users.noreply.github.com>
2025-07-09 15:44:59 +0530

    Inventory Dashboard added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,38 +10,41 @@ const Projects = () => {
       <div className="text-center text-2xl font-bold mb-6">My Projects</div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 justify-center items-center w-full">
-        {projectData.map(({ githubLink, subtitle, title, imgUrl }, index) => (
-          <div
-            className="relative rounded-md cursor-pointer overflow-hidden max-w-[270px] mx-auto bg-slate-800"
-            key={index}
-          >
-            <Image
-              src={imgUrl}
-              width={290}
-              height={270}
-              alt={title}
-              className="rounded-md object-cover h-[150px]"
-            />
-            <div className="flex flex-col items-center my-3 ">
-              <div className="font-semibold text-md text-yellow-500">
-                {title}
-              </div>
-              <div className="flex-grow h-[70px] px-2 text-center text-sm">
-                {subtitle}
-              </div>
-              <div className="flex gap-3 mb-3">
-                <Link
-                  className="btn bg-blue-500 text-white px-4 rounded-md hover:bg-blue-600 transition"
-                  href={githubLink}
-                  target="_blank"
-                  aria-label={`View ${title} on GitHub`}
-                >
-                  Github
-                </Link>
+        {projectData.map(
+          ({ githubLink, subtitle, title, imgUrl, skill }, index) => (
+            <div
+              className="relative rounded-md cursor-pointer overflow-hidden max-w-[270px] mx-auto bg-slate-800"
+              key={index}
+            >
+              <Image
+                src={imgUrl}
+                width={290}
+                height={270}
+                alt={title}
+                className="rounded-md object-cover h-[150px]"
+              />
+              <div className="flex flex-col items-center my-3 ">
+                <div className="font-semibold text-md text-yellow-500">
+                  {title}
+                </div>
+                <div className="flex-grow h-[70px] px-2 py-2 text-center text-sm">
+                  {subtitle}
+                </div>
+                <div className="flex gap-3 mb-3">
+                  <Link
+                    className="btn bg-blue-500 text-white px-4 rounded-md hover:bg-blue-600 transition"
+                    href={githubLink}
+                    target="_blank"
+                    aria-label={`View ${title} on GitHub`}
+                  >
+                    Github
+                  </Link>
+                </div>
+                <div>{skill}</div>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
--- data/projectdata.jsx
+++ data/projectdata.jsx
@@ -2,10 +2,19 @@ export const projectData = [
   {
     title: "Car Sales Dashboard",
     subtitle:
-      "This dashboard is designed to visualize and analyze key sales metrics for business decision-making.",
+      "To visualize and analyze key sales metrics for business decision-making.",
     githubLink:
       "https://github.com/kiransalve/Power-BI-Projects/tree/main/1.%20Car%20Sales%20Dashboard",
     imgUrl: "/car_sales.png",
-    skill: "Power BI",
+    skill: "Power BI, DAX",
+  },
+  {
+    title: "Inventory Dashboard",
+    subtitle:
+      "This dashboard provides a complete snapshot of inventory performance.",
+    githubLink:
+      "https://github.com/kiransalve/Power-BI-Projects/tree/main/2.%20Inventory%20Dashboard",
+    imgUrl: "/inv.png",
+    skill: "Power BI, DAX, Custom Tooltip",
   },
 ];
