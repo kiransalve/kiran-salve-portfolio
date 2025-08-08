98a071104660ceffc3356672a42ff274584e6cf8
Kiran Salve <90862340+kiransalve@users.noreply.github.com>
2025-08-08 16:01:06 +0530

    ads spent project added

--- data/projectdata.jsx
+++ data/projectdata.jsx
@@ -1,5 +1,14 @@
 export const projectData = [
   {
+    title: "Ads Prediction",
+    subtitle:
+      "This is an app that predicts product sales based on advertising spend on TV, Radio, and Newspaper.",
+    githubLink:
+      "https://github.com/kiransalve/advertising_linear_regression/tree/main",
+    imgUrl: "/ads.png",
+    skill: "Python, Linear Regression, Pandas",
+  },
+  {
     title: "Car Sales Dashboard",
     subtitle:
       "To visualize and analyze key sales metrics for business decision-making.",
